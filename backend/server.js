be7d2bb315dd0d44af53b81f32e3cb345333540b
Umer-ishfaq12 <umerishfaq488@gmail.com>
2026-05-08 06:27:17 -0700

    done

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 // const authRoutes = require('./routes/auth');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./Routes/authRoutes');
 // const medicineRoutes = require('./routes/medicines');
-const medicineRoutes = require('./routes/medicineRoutes');
+const medicineRoutes = require('./Routes/MedicineRoutes');
 // const saleRoutes = require('./routes/sales');
-const saleRoutes = require('./routes/salesRoutes');
+const saleRoutes = require('./Routes/salesroutes');
 
 const app = express();
 
